59f91bb0e6ee4e7f3cf9b6e52dd5e5d16c625fac
ProValdi <prokhorov.va@phystech.edu>
2020-10-22 21:42:17 +0300

    create proj

--- edu-project-application/src/main/java/com/ncedu/eduprojectapplication/EduProjectApplication.java
+++ edu-project-application/src/main/java/com/ncedu/eduprojectapplication/EduProjectApplication.java
@@ -7,31 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-
 @Slf4j
-// entry point for spring boot app
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ncedu.eduprojectapplication", "com.ncedu.eduprojectweb"})
 public class EduProjectApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(EduProjectApplication.class.getName());
-
     public static void main(String[] args) {
-        //SpringApplication.run(EduProjectApplication.class, args);
-
-        useF(value ->{
-            return "kd";
-        });
-        log.info("dhdhhd");
-
-    }
-
-    private static void useF(Function<Boolean, String> f) {
-        System.out.println(f.apply(true));
+        SpringApplication.run(EduProjectApplication.class, args);
     }
 
 }
-
--- edu-project-web/src/main/java/com/ncedu/eduprojectweb/TestController.java
+++ edu-project-web/src/main/java/com/ncedu/eduprojectweb/TestController.java
@@ -1,8 +1,6 @@
 package com.ncedu.eduprojectweb;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,23 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TestController.class.getName());
+    String html = "Hello, World!";
 
-    public TestController() {
-        int a = 10;
-
-        logger.info("a = {}", a);
-        logger.trace("a1 = {}", a);
-        logger.debug("a2 = {}", a);
-        logger.info("a3 = {}", a);
-        logger.warn("a4 = {}", a);
-        logger.error("a5 = {}", a);
-        // как консоль сделать
-    }
-
-    String html = "hello madafuck";
-
-    // we can return html file
     @GetMapping("/test")
     public String getTestString() {
         return html;
@@ -36,7 +19,7 @@ public class TestController {
 
     @GetMapping("/test1")
     public String getOtherString() {
-        return "fuckoff";
+        return "MIPT";
     }
 
 }
