cd350d60c1c808ffd7e5b8d23148bdd30db8956c
FilippovIvan19 <fim1902@gmail.com>
2020-11-30 14:00:11 +0300

    user delete by id, package for entities

--- knownet-impl/src/main/java/com/ncedu/knownetimpl/repository/UserRepository.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.ncedu.knownetimpl.repository;
 
-import com.ncedu.knownetimpl.model.User;
+import com.ncedu.knownetimpl.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
