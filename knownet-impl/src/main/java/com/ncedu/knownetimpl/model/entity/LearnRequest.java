14bce7c770f093edc4ace81a299112b7e74a7f04
Aleksey Ptukha <ptukha.aiu@phystech.edu>
2021-01-31 22:14:10 +0300

    Start

--- knownet-impl/src/main/java/com/ncedu/knownetimpl/model/entity/LearnRequest.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/model/entity/LearnRequest.java
@@ -1,6 +1,6 @@
 package com.ncedu.knownetimpl.model.entity;
 
-import com.ncedu.knownetimpl.model.User;
+import com.ncedu.knownetimpl.model.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -18,47 +18,47 @@ public class LearnRequest {
     @Id
     @GeneratedValue
     private Long id;
-    
+
     @ManyToOne
     @JoinColumn(name = "teacher_id", referencedColumnName = "id")
     private User teacher;
-    
+
     @ManyToOne
     @JoinColumn(name = "student_id", referencedColumnName = "id")
     private User student;
-    
+
 //    todo add foreign key constraint into table
 //    todo connect to lesson class
 //    @ManyToOne()
 //    @JoinColumn(name = "lesson_id", referencedColumnName = "id")
 //    private Lesson lesson;
-    
+
     @ColumnDefault("string default LESSON_REQUESTED")
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private Status status = Status.LESSON_REQUESTED;
-    
+
 //    @ColumnDefault("boolean default true")
 //    @Column(name = "awaiting_teacher")
 //    private Boolean awaitingTeacher = true;
-    
+
     @ColumnDefault("boolean default false")
     @Column(name = "hidden_for_teacher")
     private Boolean hiddenForTeacher = false;
-    
+
     @ColumnDefault("boolean default false")
     @Column(name = "hidden_for_student")
     private Boolean hiddenForStudent = false;
-    
+
     public Boolean isAwaitingTeacher() {
         return status == Status.LESSON_REQUESTED;
     }
-    
+
     public Boolean isAwaitingStudent() {
         return status == Status.LESSON_REQUEST_ACCEPTED;
     }
-    
-    
+
+
     public enum Status {
         LESSON_REQUESTED("Student requested for the lesson and waiting for teacher's reply"),
         LESSON_REQUEST_ACCEPTED("Teacher accepted student's request"),
@@ -67,10 +67,10 @@ public class LearnRequest {
         MEETING_CANCELED("Student decided to cancel the meeting"),
         MEETING_DISPROVED("Student declared that the meeting with teacher had NOT taken place"),
         CONNECTION_FINISHED("Request cycle was fully processed");
-        
+
         @Getter
         private final String description;
-        
+
         Status(String description) {
             this.description = description;
         }
--- knownet-impl/src/main/java/com/ncedu/knownetimpl/service/LearnRequestService.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/service/LearnRequestService.java
@@ -1,7 +1,7 @@
 package com.ncedu.knownetimpl.service;
 
 import com.ncedu.knownetimpl.model.LearnRequestBody;
-import com.ncedu.knownetimpl.model.User;
+import com.ncedu.knownetimpl.model.entity.User;
 import com.ncedu.knownetimpl.model.entity.LearnRequest;
 import com.ncedu.knownetimpl.repository.LearnRequestRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,20 +15,20 @@ import java.util.Optional;
 public class LearnRequestService {
     private final LearnRequestRepository learnRequestRepository;
     private final UserService userService;
-    
+
     public LearnRequestService(LearnRequestRepository learnRequestRepository, UserService userService) {
         this.learnRequestRepository = learnRequestRepository;
         this.userService = userService;
     }
-    
+
     public List<LearnRequest> findAll() {
         return learnRequestRepository.findAll();
     }
-    
+
     public Optional<LearnRequest> findById(Long id) {
         return learnRequestRepository.findById(id);
     }
-    
+
     public boolean deleteById(Long id) {
         boolean exists = learnRequestRepository.existsById(id);
         if (exists) {
@@ -36,25 +36,25 @@ public class LearnRequestService {
         }
         return exists;
     }
-    
+
     public List<LearnRequest> findByTeacherId(Long teacherId) {
         return learnRequestRepository.findByTeacherId(teacherId);
     }
-    
+
     public List<LearnRequest> findByStudentId(Long studentId) {
         return learnRequestRepository.findByStudentId(studentId);
     }
-    
+
     public List<LearnRequest> findActiveByTeacherId(Long teacherId) {
         return learnRequestRepository.findByTeacherIdAndHiddenForTeacherAndStatusNot(
                 teacherId, false, LearnRequest.Status.CONNECTION_FINISHED);
     }
-    
+
     public List<LearnRequest> findActiveByStudentId(Long studentId) {
         return learnRequestRepository.findByStudentIdAndHiddenForStudentAndStatusNot(
                 studentId, false, LearnRequest.Status.CONNECTION_FINISHED);
     }
-    
+
     public boolean create(LearnRequest learnRequest) {
         boolean exists = learnRequest.getId() != null && learnRequestRepository.existsById(learnRequest.getId());
         if (!exists) {
@@ -62,44 +62,44 @@ public class LearnRequestService {
         }
         return !exists;
     }
-    
+
     public boolean update(LearnRequest learnRequest) {
         Optional<LearnRequest> oldLearnRequestOpt = findById(learnRequest.getId());
         if (oldLearnRequestOpt.isPresent()) {
             LearnRequest oldLearnRequest = oldLearnRequestOpt.get();
-            
+
             oldLearnRequest.setHiddenForStudent(learnRequest.getHiddenForStudent());
             oldLearnRequest.setHiddenForTeacher(learnRequest.getHiddenForTeacher());
-            
+
             LearnRequest.Status newStatus = learnRequest.getStatus();
             if (oldLearnRequest.getStatus() != newStatus) {
                 log.debug(newStatus.getDescription());
                 oldLearnRequest.setStatus(newStatus);
             }
-            
+
             learnRequestRepository.save(oldLearnRequest);
         }
-        
+
         return oldLearnRequestOpt.isPresent();
     }
-    
+
     public LearnRequest makeFromBody(LearnRequestBody body) {
         LearnRequest learnRequest = new LearnRequest();
-    
+
         learnRequest.setId(body.getId());
         learnRequest.setHiddenForStudent(body.getHiddenForStudent());
         learnRequest.setHiddenForTeacher(body.getHiddenForTeacher());
         learnRequest.setStatus(body.getStatus());
-    
+
         Optional<User> student = userService.findById(body.getStudentId());
         Optional<User> teacher = userService.findById(body.getTeacherId());
 //        todo activate
 //        Optional<Lesson> lesson = userService.findById(body.getLessonId());
-    
+
         learnRequest.setStudent(student.orElse(new User()));
         learnRequest.setTeacher(teacher.orElse(new User()));
 //        learnRequest.setLesson(lesson.orElse(new Lesson()));
-        
+
         return learnRequest;
     }
 }
