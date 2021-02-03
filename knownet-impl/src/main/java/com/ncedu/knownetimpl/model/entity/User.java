f68caa7f026415df825c9ea4a8b325641d026fba
Aleksey Ptukha <ptukha.aiu@phystech.edu>
2021-02-03 19:45:48 +0300

    Boclajan

--- knownet-impl/src/main/java/com/ncedu/knownetimpl/model/entity/User.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/model/entity/User.java
@@ -17,48 +17,67 @@ public class User {
 
     @Column(name = "login", nullable = false, unique = true)
     private String login;
-    
+
     @Column(name = "encoded_password")
     private String encodedPassword;
-    
+
     @Column(name = "first_name")
     private String firstName;
-    
+
     @Column(name = "last_name")
     private String lastName;
-    
+
     @Column(name = "rating")
     private Double rating;
-    
+
     @Column(name = "points")
     private Integer points;
-    
+
 //    private Integer additionalBuyingCost;
 //    private Integer additionalSellingCost;
-    
+
     @Column(name = "user_group")
     private String group;
-    
+
 //    private UserSettings settings;
 //    private Status status;
 //    private Image photo;
-    
+
     @Column(name = "email")
     private String email;
-    
+
     @Column(name = "vk_link")
     private String vkLink;
-    
+
     @Column(name = "telegram_link")
     private String telegramLink;
-    
+
     @Column(name = "phone_number")
     private String phoneNumber;
-    
+
     @Column(name = "attended_count")
     private Integer attendedCount;
-    
+
     @Column(name = "conducted_count")
     private Integer conductedCount;
-    
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public String getEncodedPassword() {
+    return encodedPassword;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
 }
--- knownet-impl/src/main/java/com/ncedu/knownetimpl/service/LearnRequestService.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/service/LearnRequestService.java
@@ -7,7 +7,6 @@ import com.ncedu.knownetimpl.model.entity.LearnRequest;
 import com.ncedu.knownetimpl.repository.LearnRequestRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
