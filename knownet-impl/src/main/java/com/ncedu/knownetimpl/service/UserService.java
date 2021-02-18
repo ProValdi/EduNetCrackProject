32b4154fc6153342f2ea4a83bdfb6d6fcab933b8
Aleksey Ptukha <ptukha.aiu@phystech.edu>
2021-02-18 13:27:49 +0300

    y

--- knownet-impl/src/main/java/com/ncedu/knownetimpl/service/UserService.java
+++ knownet-impl/src/main/java/com/ncedu/knownetimpl/service/UserService.java
@@ -2,9 +2,6 @@ package com.ncedu.knownetimpl.service;
 
 import com.ncedu.knownetimpl.model.entity.User;
 import com.ncedu.knownetimpl.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
